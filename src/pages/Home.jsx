3e9b15d116f68f3bf320e7f503b02e09cb201db1
Professor SESI/SENAI <Professor@SN-358625.edu.fiesc.com.br>
2025-03-27 14:06:01 -0300

    add hook navigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,22 @@
 import Navbar from "../components/Navbar"
 import { useContext, useState } from "react"
 import { GlobalContext } from "../contexts/GlobalContext"
+import { useNavigate } from 'react-router-dom';
 function Home() {
     const {usuarioLogado} = useContext(GlobalContext)
- 
+    const navigate = useNavigate(); // Hook para navegação 
+
+    function logar(){
+      navigate('/final');
+    }
 
   return (
     <div>
         <Navbar />
       <h1>Página home do site</h1>
       <p>Olá {usuarioLogado}</p>
+
+      <button onClick={logar}>Login</button>
 
     </div>
   )
