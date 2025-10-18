c1128363793cf3de751b59bd521b81104da7a9da
rafaellindemann <rafaelld@gmail.com>
2025-10-18 16:15:15 -0300

    sync

--- src/pages/Final.jsx
+++ src/pages/Final.jsx
@@ -2,11 +2,13 @@ import Navbar from "../components/Navbar"
 
 function Final() {
   return (
-    <div>
-        <Navbar />
-      <h1>Final</h1>
-      <p>Você chegou ao fim da internet, por enquanto...</p>
-    </div>
+      <GerencialContextProvider>
+        <div>
+            <Navbar />
+          <h1>Final</h1>
+          <p>Você chegou ao fim da internet, por enquanto...</p>
+        </div>
+      </GerencialContextProvider>
   )
 }
 
